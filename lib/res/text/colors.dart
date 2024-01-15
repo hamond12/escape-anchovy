38b63f08ac3c369ed90dc69623554785d5a6ef5c
hamond12 <chicken010208@gmail.com>
2024-01-15 11:31:09 +0900

    style: 다크모드 색상 정의

--- lib/res/text/colors.dart
+++ lib/res/text/colors.dart
@@ -7,22 +7,22 @@ class LightModeColors {
   static const Color green = Color(0xFF009300);
   static const Color gold = Color(0xFFFFD700);
   static const Color darkGold = Color(0xFFF5CD14);
-  static const Color gray = Color(0xFF77717A);
-  static const Color lightGray = Color(0xFFF2F2F2);
-  static const Color darkGray = Color(0xFF2A2A2A);
+  static const Color dark1 = Color(0xFF202020);
+  static const Color dark2 = Color(0xFF3E3E3E);
+  static const Color dark3 = Color(0xFF8A848D);
+  static const Color dark4 = Color(0xFFF2F2F2);
 }
 
-// todo: 색상변경 필요
 class DarkModeColors {
   static const Color background = Color(0xFF000000);
-  static const Color blue = Color(0xFF5B84FF);
-  static const Color red = Color(0xFFFE3F2F);
-  static const Color green = Color(0xFF009300);
-  static const Color gold = Color(0xFFFFD700);
-  static const Color darkGold = Color(0xFFF5CD14);
-  static const Color gray = Color(0xFF77717A);
-  static const Color lightGray = Color(0xFFF2F2F2);
-  static const Color darkGray = Color(0xFF2A2A2A);
+  static const Color blue = Color(0xFF79A2FF);
+  static const Color red = Color(0xFFFF5D4D);
+  static const Color green = Color(0xFF1EB11E);
+  static const Color darkGold = Color(0xFFE9C332);
+  static const Color dark1 = Color(0xFFF2F2F2);
+  static const Color dark2 = Color(0xFFCDCDCD);
+  static const Color dark3 = Color(0xFF948E97);
+  static const Color dark4 = Color(0xFF2A2A2A);
 }
 
 extension ThemeExtension on BuildContext {
